ec56a0e34602df55584ad047240d8eefcc009c0b
kidsoffuture230@gmail.com <kidsoffuture230@gmail.com>
2021-03-12 17:30:53 +0530

    added slingshot

---
+++ Slingshot.js
@@ -0,0 +1,19 @@
+class Slingshot
+{
+    constructor(bodyA,pointB)
+    {
+        var Option={bodyA:bodyA,pointB:pointB,stiffness:0.04,length:10}
+        this.Slingshot=Matter.Constraint.create(Option);
+        World.add(world,this.Slingshot);
+
+    }
+
+    display(){
+        push();
+        strokeWeight(5);
+        stroke("orange");
+        line(this.Slingshot.bodyA.position.x,this.Slingshot.bodyA.position.y,this.Slingshot.pointB.x,this.Slingshot.pointB.y);
+        pop();
+    }
+    
+}
